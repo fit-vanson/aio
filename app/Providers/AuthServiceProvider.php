a9630f1fdd203d32be5f77f641b1b916272f264e
Admin <fit.vanson@gmail.com>
2021-06-02 16:51:38 +0700

    define

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Policies\DaPolicy;
+use App\Policies\DevPolicy;
+use App\Policies\Ga_devPolicy;
+use App\Policies\GaPolicy;
+use App\Policies\PermissionPolicy;
+use App\Policies\ProjectPolicy;
+use App\Policies\RolePolicy;
+use App\Policies\TemplatePolicy;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use function PHPUnit\Framework\callback;
@@ -38,80 +47,84 @@ class AuthServiceProvider extends ServiceProvider
     }
 
     public function defineProject(){
-        Gate::define('project-index',callback:'App\Policies\ProjectPolicy@index');
-        Gate::define('project-show',callback:'App\Policies\ProjectPolicy@show');
-        Gate::define('project-add',callback:'App\Policies\ProjectPolicy@add');
-        Gate::define('project-edit',callback:'App\Policies\ProjectPolicy@edit');
-        Gate::define('project-update',callback:'App\Policies\ProjectPolicy@update');
-        Gate::define('project-delete',callback:'App\Policies\ProjectPolicy@delete');
+
+        Gate::define('project-index', [ProjectPolicy::class, 'index']);
+        Gate::define('project-show', [ProjectPolicy::class, 'show']);
+        Gate::define('project-add', [ProjectPolicy::class, 'add']);
+        Gate::define('project-edit', [ProjectPolicy::class, 'edit']);
+        Gate::define('project-update', [ProjectPolicy::class, 'update']);
+        Gate::define('project-delete', [ProjectPolicy::class, 'delete']);
+
     }
 
     public function defineDu_an(){
-        Gate::define('du_an-index',callback:'App\Policies\DaPolicy@index');
-        Gate::define('du_an-show',callback:'App\Policies\DaPolicy@show');
-        Gate::define('du_an-add',callback:'App\Policies\DaPolicy@add');
-        Gate::define('du_an-edit',callback:'App\Policies\DaPolicy@edit');
-        Gate::define('du_an-update',callback:'App\Policies\DaPolicy@update');
-        Gate::define('du_an-delete',callback:'App\Policies\DaPolicy@delete');
+        Gate::define('du_an-index', [DaPolicy::class, 'index']);
+        Gate::define('du_an-show', [DaPolicy::class, 'show']);
+        Gate::define('du_an-add', [DaPolicy::class, 'add']);
+        Gate::define('du_an-edit', [DaPolicy::class, 'edit']);
+        Gate::define('du_an-update', [DaPolicy::class, 'update']);
+        Gate::define('du_an-delete', [DaPolicy::class, 'delete']);
     }
 
     public function defineTemplate(){
-        Gate::define('template-index',callback:'App\Policies\TemplatePolicy@index');
-        Gate::define('template-show',callback:'App\Policies\TemplatePolicy@show');
-        Gate::define('template-add',callback:'App\Policies\TemplatePolicy@add');
-        Gate::define('template-edit',callback:'App\Policies\TemplatePolicy@edit');
-        Gate::define('template-update',callback:'App\Policies\TemplatePolicy@update');
-        Gate::define('template-delete',callback:'App\Policies\TemplatePolicy@delete');
+        Gate::define('template-index', [TemplatePolicy::class, 'index']);
+        Gate::define('template-show', [TemplatePolicy::class, 'show']);
+        Gate::define('template-add', [TemplatePolicy::class, 'add']);
+        Gate::define('template-edit', [TemplatePolicy::class, 'edit']);
+        Gate::define('template-update', [TemplatePolicy::class, 'update']);
+        Gate::define('template-delete', [TemplatePolicy::class, 'delete']);
     }
 
     public function defineGadev(){
-        Gate::define('gadev-index',callback:'App\Policies\Ga_devPolicy@index');
-        Gate::define('gadev-show',callback:'App\Policies\Ga_devPolicy@show');
-        Gate::define('gadev-add',callback:'App\Policies\Ga_devPolicy@add');
-        Gate::define('gadev-edit',callback:'App\Policies\Ga_devPolicy@edit');
-        Gate::define('gadev-update',callback:'App\Policies\Ga_devPolicy@update');
-        Gate::define('gadev-delete',callback:'App\Policies\Ga_devPolicy@delete');
+        Gate::define('gadev-index', [Ga_devPolicy::class, 'index']);
+        Gate::define('gadev-show', [Ga_devPolicy::class, 'show']);
+        Gate::define('gadev-add', [Ga_devPolicy::class, 'add']);
+        Gate::define('gadev-edit', [Ga_devPolicy::class, 'edit']);
+        Gate::define('gadev-update', [Ga_devPolicy::class, 'update']);
+        Gate::define('gadev-delete', [Ga_devPolicy::class, 'delete']);
     }
     public function defineGa(){
-        Gate::define('ga-index',callback:'App\Policies\GaPolicy@index');
-        Gate::define('ga-show',callback:'App\Policies\GaPolicy@show');
-        Gate::define('ga-add',callback:'App\Policies\GaPolicy@add');
-        Gate::define('ga-edit',callback:'App\Policies\GaPolicy@edit');
-        Gate::define('ga-update',callback:'App\Policies\GaPolicy@update');
-        Gate::define('ga-delete',callback:'App\Policies\GaPolicy@delete');
+        Gate::define('ga-index', [GaPolicy::class, 'index']);
+        Gate::define('ga-show', [GaPolicy::class, 'show']);
+        Gate::define('ga-add', [GaPolicy::class, 'add']);
+        Gate::define('ga-edit', [GaPolicy::class, 'edit']);
+        Gate::define('ga-update', [GaPolicy::class, 'update']);
+        Gate::define('ga-delete', [GaPolicy::class, 'delete']);
     }
     public function defineDev(){
-        Gate::define('dev-index',callback:'App\Policies\DevPolicy@index');
-        Gate::define('dev-show',callback:'App\Policies\DevPolicy@show');
-        Gate::define('dev-add',callback:'App\Policies\DevPolicy@add');
-        Gate::define('dev-edit',callback:'App\Policies\DevPolicy@edit');
-        Gate::define('dev-update',callback:'App\Policies\DevPolicy@update');
-        Gate::define('dev-delete',callback:'App\Policies\DevPolicy@delete');
+
+        Gate::define('dev-index', [DevPolicy::class, 'index']);
+        Gate::define('dev-show', [DevPolicy::class, 'show']);
+        Gate::define('dev-add', [DevPolicy::class, 'add']);
+        Gate::define('dev-edit', [DevPolicy::class, 'edit']);
+        Gate::define('dev-update', [DevPolicy::class, 'update']);
+        Gate::define('dev-delete', [DevPolicy::class, 'delete']);
     }
 
     public function defineVaitro(){
-        Gate::define('vai_tro-index',callback:'App\Policies\RolePolicy@index');
-        Gate::define('vai_tro-show',callback:'App\Policies\RolePolicy@show');
-        Gate::define('vai_tro-add',callback:'App\Policies\RolePolicy@add');
-        Gate::define('vai_tro-edit',callback:'App\Policies\RolePolicy@edit');
-        Gate::define('vai_tro-update',callback:'App\Policies\RolePolicy@update');
-        Gate::define('vai_tro-delete',callback:'App\Policies\RolePolicy@delete');
+        Gate::define('vai_tro-index', [RolePolicy::class, 'index']);
+        Gate::define('vai_tro-show', [RolePolicy::class, 'show']);
+        Gate::define('vai_tro-add', [RolePolicy::class, 'add']);
+        Gate::define('vai_tro-edit', [RolePolicy::class, 'edit']);
+        Gate::define('vai_tro-update', [RolePolicy::class, 'update']);
+        Gate::define('vai_tro-delete', [RolePolicy::class, 'delete']);
     }
     public function defineUser(){
-        Gate::define('user-index',callback:'App\Policies\UserPolicy@index');
-        Gate::define('user-show',callback:'App\Policies\UserPolicy@show');
-        Gate::define('user-add',callback:'App\Policies\UserPolicy@add');
-        Gate::define('user-edit',callback:'App\Policies\UserPolicy@edit');
-        Gate::define('user-update',callback:'App\Policies\UserPolicy@update');
-        Gate::define('user-delete',callback:'App\Policies\UserPolicy@delete');
+        Gate::define('user-index', [UserPolicy::class, 'index']);
+        Gate::define('user-show', [UserPolicy::class, 'show']);
+        Gate::define('user-add', [UserPolicy::class, 'add']);
+        Gate::define('user-edit', [UserPolicy::class, 'edit']);
+        Gate::define('user-update', [UserPolicy::class, 'update']);
+        Gate::define('user-delete', [UserPolicy::class, 'delete']);
     }
     public function definePhan_quyen(){
-        Gate::define('phan_quyen-index',callback:'App\Policies\PermissionPolicy@index');
-        Gate::define('phan_quyen-show',callback:'App\Policies\PermissionPolicy@show');
-        Gate::define('phan_quyen-add',callback:'App\Policies\PermissionPolicy@add');
-        Gate::define('phan_quyen-edit',callback:'App\Policies\PermissionPolicy@edit');
-        Gate::define('phan_quyen-update',callback:'App\Policies\PermissionPolicy@update');
-        Gate::define('phan_quyen-delete',callback:'App\Policies\PermissionPolicy@delete');
+        Gate::define('phan_quyen-index', [PermissionPolicy::class, 'index']);
+        Gate::define('phan_quyen-show', [PermissionPolicy::class, 'show']);
+        Gate::define('phan_quyen-add', [PermissionPolicy::class, 'add']);
+        Gate::define('phan_quyen-edit', [PermissionPolicy::class, 'edit']);
+        Gate::define('phan_quyen-update', [PermissionPolicy::class, 'update']);
+        Gate::define('phan_quyen-delete', [PermissionPolicy::class, 'delete']);
+
     }
 
 
