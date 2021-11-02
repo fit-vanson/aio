b5b8100b786344b351137cf351b9d2ae18c9be53
Administrator <fit.vanson@gmail.com>
2021-11-02 10:25:21 +0700

    update Script

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -119,7 +119,7 @@
                             </li>
 
                             <li>
-                                <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-code"></i> <span>Fake Device<span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span> </a>
+                                <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-code"></i> <span>Script<span class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span> </a>
                                 <ul class="submenu">
                                     @can('script-index')
                                         <li><a href="{{route('script.index')}}">Quản lý Script</a></li>
