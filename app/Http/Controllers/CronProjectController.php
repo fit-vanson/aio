2d84cff548d34eb0d2caeaa9b8d5f740072cbb14
Administrator <fit.vanson@gmail.com>
2021-11-16 14:10:34 +0700

    update template

--- app/Http/Controllers/CronProjectController.php
+++ app/Http/Controllers/CronProjectController.php
@@ -79,22 +79,76 @@ class CronProjectController extends Controller
 
     public function getPackage(Request $request)
     {
-        if ($request->id) {
-            $package = $request->id;
-            $appInfo = new GPlayApps();
-            $existApp =  $appInfo->existsApp($package);
-            if($existApp){
-                $appInfo = $appInfo->getAppInfo($package);
-                echo $appInfo->getIcon();
-                echo ' | '. $appInfo->getName();
-                echo ' | '. $appVersion = ($appInfo->getAppVersion() != null)  ? number_format($appInfo->getAppVersion()) : 'null';
-                echo ' | '. $score = ($appInfo->getScore() != null)  ? number_format($appInfo->getScore(),1) : 'null';
-                echo ' | '. $install =  ($appInfo->getInstalls() != null)  ? number_format($appInfo->getInstalls()) : 'null';
-                return;
+        if ($request->id){
+            $pattern = "/^(com).*/i";
+            $regex = preg_match($pattern,$request->id);
+            if($regex){
+                $package = $request->id;
+                $appInfo = new GPlayApps();
+                $existApp =  $appInfo->existsApp($package);
+                if($existApp){
+                    $appInfo = $appInfo->getAppInfo($package);
+                    echo $appInfo->getIcon();
+                    echo ' | '. $appInfo->getName();
+                    echo ' | '. $appVersion = ($appInfo->getAppVersion() != null)  ? $appInfo->getAppVersion() : 'null';
+                    echo ' | '. $score = ($appInfo->getScore() != null)  ? number_format($appInfo->getScore(),1) : 'null';
+                    echo ' | '. $install =  ($appInfo->getInstalls() != null)  ? number_format($appInfo->getInstalls()) : 'null';
+                    return;
+                }else{
+                    return 'null';
+                }
             }else{
-                return 'null';
+                $ch = curl_init("https://web-dra.hispace.dbankcloud.cn/uowap/index?method=internal.getTabDetail&uri=app|".$request->id); // such as http://example.com/example.xml
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                $data = curl_exec($ch);
+                $data = json_decode($data,true);
+                if(count($data['layoutData'])>0)
+                {
+                    echo '<a href="'.$data['layoutData'][0]['dataList'][0]['downurl'].'">'.$data['layoutData'][0]['dataList'][0]['name'].'</a>';
+                    return;
+                }else{
+                    return 'null';
+                }
+                curl_close($ch);
+                return;
             }
+
+
         }
         return URL::current().'?id=xyz';
+    }
+
+
+
+
+    function array_search_id($search_value, $array, $id_path) {
+
+        if(is_array($array) && count($array) > 0) {
+
+            foreach($array as $key => $value) {
+
+                $temp_path = $id_path;
+
+                // Adding current key to search path
+                array_push($temp_path, $key);
+
+                // Check if this value is an array
+                // with atleast one element
+                if(is_array($value) && count($value) > 0) {
+                    $res_path = $this->array_search_id(
+                        $search_value, $value, $temp_path);
+
+                    if ($res_path != null) {
+                        return $res_path;
+                    }
+                }
+                else if($value == $search_value) {
+                    return join(" --> ", $temp_path);
+                }
+            }
+        }
+
+        return null;
     }
 }
