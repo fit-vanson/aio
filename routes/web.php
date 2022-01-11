075ec04c5ad5b40c896c37c092bb801189f30029
Administrator <fit.vanson@gmail.com>
2022-01-11 11:50:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,27 @@ Route::get('/clear-cache',function (){
     echo  Artisan::call('optimize:clear');
 });
 
+Route::get('routes', function () {
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo "<tr>";
+    echo "<td width='10%'><h4>HTTP Method</h4></td>";
+    echo "<td width='10%'><h4>Route</h4></td>";
+    echo "<td width='10%'><h4>Name</h4></td>";
+    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
+    echo "</tr>";
+    foreach ($routeCollection as $value) {
+        echo "<tr>";
+        echo "<td>" . $value->methods()[0] . "</td>";
+        echo "<td>" . $value->uri() . "</td>";
+        echo "<td>" . $value->getName() . "</td>";
+        echo "<td>" . $value->getActionName() . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+});
+
 Route::group(['prefix'=>'login','middleware'=>'CheckUser'], function (){
     Route::get('/',[HomeController::class,'getLogin']);
     Route::post('/',[HomeController::class,'postLogin'])->name('login');
