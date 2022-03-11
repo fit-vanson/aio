d76220c0c267fd4ae0535f4f30152f09cd1f60ce
Administrator <fit.vanson@gmail.com>
2022-03-11 09:10:22 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -87,7 +87,7 @@
             </div>
         </div>
         <div class="col-xl-3 col-md-6">
-            <div class="card mini-stat text-white" style="background: #ffc107 !important;">
+            <div class="card mini-stat text-white" style="background: #7baba3 !important;">
                 <div class="card-body">
                     <div class="mb-4">
                         <div class="float-left mini-stat-img mr-4">
