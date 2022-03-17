94417561bf2e3cc1438ce7f1a3b7093be631c280
Administrator <fit.vanson@gmail.com>
2022-03-17 14:02:00 +0700

    update

--- app/Http/Controllers/TemplatePreviewController.php
+++ app/Http/Controllers/TemplatePreviewController.php
@@ -19,7 +19,7 @@ class TemplatePreviewController extends Controller
 
     public function getIndex(Request $request)
     {
-//        dd((1));
+
         $draw = $request->get('draw');
         $start = $request->get("start");
         $rowperpage = $request->get("length"); // total number of rows per page
@@ -36,33 +36,15 @@ class TemplatePreviewController extends Controller
 
 
         // Total records
-        $totalRecords = Template::select('count(*) as allcount')->count();
-        $totalRecordswithFilter = Template::select('count(*) as allcount')
-            ->where('template', 'like', '%' . $searchValue . '%')
-            ->orwhere('template_name', 'like', '%' . $searchValue . '%')
-            ->orWhere('ver_build', 'like', '%' . $searchValue . '%')
-            ->orWhere('Chplay_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Amazon_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Samsung_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Xiaomi_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Oppo_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Vivo_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Huawei_category', 'like', '%' . $searchValue . '%')
+        $totalRecords = TemplatePreview::select('count(*) as allcount')->count();
+        $totalRecordswithFilter = TemplatePreview::select('count(*) as allcount')
+            ->where('tp_name', 'like', '%' . $searchValue . '%')
             ->count();
 
 
         // Get records, also we have included search filter as well
-        $records = Template::orderBy($columnName, $columnSortOrder)
-            ->where('template', 'like', '%' . $searchValue . '%')
-            ->orwhere('template_name', 'like', '%' . $searchValue . '%')
-            ->orWhere('ver_build', 'like', '%' . $searchValue . '%')
-            ->orWhere('Chplay_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Amazon_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Samsung_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Xiaomi_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Oppo_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Vivo_category', 'like', '%' . $searchValue . '%')
-            ->orWhere('Huawei_category', 'like', '%' . $searchValue . '%')
+        $records = TemplatePreview::orderBy($columnName, $columnSortOrder)
+            ->where('tp_name', 'like', '%' . $searchValue . '%')
             ->select('*')
             ->skip($start)
             ->take($rowperpage)
@@ -74,188 +56,14 @@ class TemplatePreviewController extends Controller
             $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$record->id.'" data-original-title="Check" class="btn btn-info checkDataTemplate"><i class="ti-file"></i></a>';
             $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$record->id.'" data-original-title="Delete" class="btn btn-danger deleteTemplate"><i class="ti-trash"></i></a>';
 
-
-            $project = DB::table('ngocphandang_template')
-                ->join('ngocphandang_project','ngocphandang_template.id','=','ngocphandang_project.template')
-                ->where('ngocphandang_project.template',$record->id)
-                ->count();
-
-            $template = '<p style="margin: 0"><b>'.$record->template_name.'</b></p>
-                            <a href="/project?q=template&id='.$record->id.'"> <span>'.$record->template.' - ('.$project.')</span></a>
-                            <p class="text-muted" style="margin: 0">'.$record->ver_build.'</p>';
-
-
-            if($record['script_copy'] !== Null){
-                $script_copy = "<span style='color:green;'> Script_copy</span> - ";
-            } else {
-                $script_copy = "<span style='color:red;'> Script_copy</span> - ";
-            }
-            if($record['script_img'] !== Null){
-                $script_img = "<span style='color:green;'> IMG</span> - ";
-            } else {
-                $script_img = "<span style='color:red;'> IMG</span> - ";
-            }
-            if($record['script_svg2xml'] !== Null){
-                $script_svg2xml = "<span style='color:green;'> svg2xml</span> - ";
-            } else {
-                $script_svg2xml = "<span style='color:red;'> svg2xml</span> - ";
-            }
-            if($record['script_file'] !== Null){
-                $script_file = "<span style='color:green;'> File</span>";
-            } else {
-                $script_file = "<span style='color:red;'> File</span>";
-            }
-            $script = $script_copy . $script_img. $script_svg2xml .$script_file;
-
-            $ads = json_decode($record->ads,true);
-
-            if(isset($ads['ads_id'])){
-                $ads_id = "<span style='color:green;'> Id</span> - ";
-            }else{
-                $ads_id = "<span style='color:red;'> Id</span> - ";
-            }
-
-            if(isset($ads['ads_banner'])){
-                $ads_banner = "<span style='color:green;'> Banner</span> - ";
-            }else{
-                $ads_banner = "<span style='color:red;'> Banner</span> - ";
-            }
-
-            if(isset($ads['ads_inter'])){
-                $ads_inter = "<span style='color:green;'> Inter</span> - ";
-            }else{
-                $ads_inter = "<span style='color:red;'> Inter</span> - ";
-            }
-
-            if(isset($ads['ads_reward'])){
-                $ads_reward = "<span style='color:green;'> Reward</span> - ";
-            }else{
-                $ads_reward = "<span style='color:red;'> Reward</span> - ";
-            }
-
-            if(isset($ads['ads_native'])){
-                $ads_native = "<span style='color:green;'> Native</span> - ";
-            }else{
-                $ads_native = "<span style='color:red;'> Native</span> - ";
-            }
-
-            if(isset($ads['ads_open'])){
-                $ads_open = "<span style='color:green;'> Open</span> - ";
-            }else{
-                $ads_open = "<span style='color:red;'> Open</span> - ";
-            }
-
-            if(isset($ads['ads_start'])){
-                $ads_start = "<span style='color:green;'> Start</span>";
-            }else{
-                $ads_start = "<span style='color:red;'> Start</span>";
-            }
-            $ads ='<br>'.$ads_id.$ads_banner.$ads_inter.$ads_reward.$ads_native.$ads_open.$ads_start;
-
-
-            if($record->convert_aab != 0){
-                $convert_aab = '<br>'. "<span style='color:green;'> Aab</span>";
-            }else{
-                $convert_aab = '<br>'. "<span style='color:red;'> Aab</span>";
-            }
-
-            if($record->startus == 0){
-                $startus = '<br>'. "<span style='color:green;'> Status</span>";
-            }else{
-                $startus = '<br>'. "<span style='color:red;'> Status</span>";
-            }
-
-            if($record->time_create == 0 ){
-                $time_create =   null;
-            }else{
-                $time_create =  date( 'd/m/Y',$record->time_create);
-            }
-
-            if($record->time_update == 0 ){
-                $time_update =   null;
-            }else{
-                $time_update =  date( 'd/m/Y',$record->time_update);
-            }
-
-            if($record->time_get == 0 ){
-                $time_get =   null;
-            }else{
-                $time_get =  date( 'd/m/Y',$record->time_get);
-            }
-            if(isset($record->Chplay_category)){
-                $Chplay_category = 'CH Play: '.$record->Chplay_category;
-            }else{
-                $Chplay_category ='';
-            }
-
-            if(isset($record->Amazon_category)){
-                $Amazon_category = 'Amazon: '.$record->Amazon_category;
-            }else{
-                $Amazon_category ='';
-            }
-
-            if(isset($record->Samsung_category)){
-                $Samsung_category = 'Samsung: '.$record->Samsung_category;
-            }else{
-                $Samsung_category ='';
-            }
-
-            if(isset($record->Xiaomi_category)){
-                $Xiaomi_category = 'Xiaomi: '.$record->Xiaomi_category;
-            }else{
-                $Xiaomi_category ='';
-            }
-
-            if(isset($record->Oppo_category)){
-                $Oppo_category = 'Oppo: '.$record->Oppo_category;
-            }else{
-                $Oppo_category ='';
-            }
-
-            if(isset($record->Vivo_category)){
-                $Vivo_category = 'Vivo: '.$record->Vivo_category;
-            }else{
-                $Vivo_category ='';
-            }
-
-            if(isset($record->Huawei_category)){
-                $Huawei_category = 'Huawei: '.$record->Huawei_category;
-            }else{
-                $Huawei_category ='';
-            }
-
-
-            if ($record->link_chplay !== null){
-                $link= "<a  target= _blank href='$record->link_chplay'>Link</a>";
-            }
-            else{
-                $link = null;
-            }
-
-            if(isset($record->template_logo)){
-                if (isset($record->link_store_vietmmo)){
-                    $logo = "<a href='".$record->link_store_vietmmo."' target='_blank'>  <img class='rounded mx-auto d-block'  width='100px'  height='100px'  src='../uploads/template/$record->template/thumbnail/$record->template_logo'></a>";
-                }else{
-                    $logo = "<img class='rounded mx-auto d-block'  width='100px'  height='100px'  src='../uploads/template/$record->template/thumbnail/$record->template_logo'>";
-                }
-            }else{
-                $logo = '<img class="rounded mx-auto d-block" width="100px" height="100px" src="assets\images\logo-sm.png">';
-            }
-
-//            $template_apk  = $record->template_apk ?  " <a href='/file-manager/ProjectApk/$record->template_apk' target='_blank' </a>" : '1111';
-            $template_apk   = $record->template_apk ?  '<a href="/file-manager/download?disk=File%20Manager&path=TemplateApk%2F/'.$record->template_apk.'" class="badge badge-success" style="font-size: 12px">APK</a>' : '<span  class="badge badge-danger" style="font-size: 12px">APK</span>';
-            $template_data  = $record->template_data ? '<a href="/file-manager/TemplateData/'.$record->template_data.'" class="badge badge-success" style="font-size: 12px">Data</a>' : '<span  class="badge badge-danger" style="font-size: 12px">Data</span>';
-
-
-
             $data_arr[] = array(
-                "logo" => $logo,
-                "template" => $template. '<br>'.$link.$template_apk.'  ' .$template_data ,
-                "category"=>$Chplay_category.'<br>'.$Amazon_category.'<br>'.$Samsung_category.'<br>'.$Xiaomi_category.'<br>'.$Oppo_category.'<br>'.$Vivo_category.'<br>'.$Huawei_category,
-                "script" => $script.$ads.$convert_aab.$startus.'<br>Package: '.$record->package,
-                "time_create"=> $time_create,
-                "time_update"=> $time_update,
-                "time_get"=> $time_get,
+//                "logo" => $logo,
+                "tp_name" => $record->tp_name,
+                "tp_number" => $record->tp_number,
+                "tp_sc" => $record->tp_sc,
+                "tp_size" => $record->tp_size,
+                "tp_start" => $record->tp_start,
+
                 "action"=> $btn,
             );
         }
---
+++ resources/views/modals/template-preview.blade.php
@@ -0,0 +1,73 @@
+
+<div class="modal fade bd-example-modal-xl" id="ajaxModel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="modelHeading"></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <form id="templatePreviewForm" name="templatePreviewForm" enctype="multipart/form-data" class="form-horizontal">
+                    <input type="hidden" name="tp_id" id="tp_id">
+
+                    <div>
+{{--                        <div data-repeater-item="" class="row">--}}
+{{--                            <div class="form-group col-lg-3">--}}
+{{--                                <label>Logo</label>--}}
+{{--                                <input  id="logo" type="file" name="logo" class="form-control" hidden onchange="changeImg(this)" accept="image/*">--}}
+{{--                                <img id="avatar" class="thumbnail" width="100px" src="img/logo.png">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group col-lg-3">--}}
+{{--                                <label for="name">DATA</label><p></p>--}}
+{{--                                <input type="file" name="template_data" id="template_data" class="filestyle" data-buttonname="btn-secondary" accept=".zip">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group col-lg-3">--}}
+{{--                                <label for="name">APK</label><p></p>--}}
+{{--                                <input type="file" name="template_apk" id="template_apk" class="filestyle" data-buttonname="btn-secondary" accept=".apk">--}}
+{{--                            </div>--}}
+{{--                            <div class="form-group col-lg-3 ">--}}
+{{--                                <label for="name">Tên Template</label>--}}
+{{--                                <input type="text" id="template_name" name="template_name" class="form-control">--}}
+{{--                            </div>--}}
+
+{{--                        </div>--}}
+                        <div class="row">
+                            <div class="form-group col-lg-6">
+                                <label for="name">Tên Template <span style="color: red">*</span></label>
+                                <input type="text" id="tp_name" name="tp_name" class="form-control" required>
+                            </div>
+                            <div class="form-group col-lg-6 ">
+                                <label for="name">Number SC</label>
+                                <input type="number" id="tp_number" name="tp_number" class="form-control" required>
+                            </div>
+                        </div>
+                        <div data-repeater-item="" class="row">
+                            <div class="form-group col-lg-4">
+                                <label for="name">File SC</label><p></p>
+                                <input type="file" name="tp_sc" id="tp_sc" class="filestyle" data-buttonname="btn-secondary" accept=".zip,.rar" />
+                            </div>
+                            <div class="form-group col-lg-4 ">
+                                <label for="name">Size</label>
+                                <input type="text" id="tp_size" name="tp_size" class="form-control" required>
+                            </div>
+                            <div class="form-group col-lg-4 ">
+                                <label for="name">Start</label>
+                                <input type="text" id="tp_start" name="tp_start" class="form-control" required>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-offset-2 col-sm-10">
+                        <button type="submit" class="btn btn-primary" id="saveBtn" value="create">Save changes
+                        </button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
