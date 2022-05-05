b70bad4591881afadabf105b15e8c72c0310cd28
Administrator <fit.vanson@gmail.com>
2022-05-05 13:57:56 +0700

    update

--- app/Models/Ga_dev.php
+++ app/Models/Ga_dev.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-use Awobaz\Compoships\Compoships;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ga_dev extends Model
 {
     use HasFactory;
-    use Compoships;
-
     public $timestamps = false;
     protected $table = 'ngocphandang_gadev';
     protected $fillable = [
